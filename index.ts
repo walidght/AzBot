7a5c4beb0cf5fbf6d9aee4c76690b1fc7bd4784a
BennounaMehdi <Bennouna.Mehdi@hotmail.com>
2021-10-10 12:12:48 +0100

    added WOKcommands and the base for Slash commands

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import DiscordJs, { Intents } from "discord.js";
+import WOKCommands from "wokcommands";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +10,12 @@ const client = new DiscordJs.Client({
 
 client.on("ready", () => {
   console.log("AzBot-Beta Online");
+
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "commands"),
+    typeScript: true,
+    testServers: ["895593257080344656"],
+  });
 });
 
 client.login(process.env.TOKEN);
